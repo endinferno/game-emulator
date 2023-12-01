7d13903998129454c84202a4a9e737ab15740b18
Edward C <censhaofeng@sjtu.edu.cn>
2023-12-01 17:31:41 +0800

    feat: support choose emulator type

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,39 @@
+#include "CPU6502.hpp"
 #include "Chip8App.hpp"
 #include "Logger.hpp"
+#include "NesReader.hpp"
+
+void PrintUsage()
+{
+    INFO("Usage: GameEmulator <type> <rom>");
+}
+
+void RunEmulator(const std::string& emulatorType, const std::string& romPath)
+{
+    DEBUG("Emulator Type: \"{}\" Rom Path: \"{}\"", emulatorType, romPath);
+    if (emulatorType == "nes") {
+        auto nesReader = std::make_shared<NesReader>(romPath);
+        CPU6502 cpu(nesReader);
+        INFO("{}", nesReader->ToString());
+        INFO("{}", cpu.ToString());
+    }
+    else if (emulatorType == "chip8") {
+        Chip8App app("CHIP8 Emulator", 64, 32);
+        app.LoadRom(romPath);
+        app.Run();
+    }
+    else {
+        ERROR("Unknown emulator type: {}", emulatorType);
+    }
+}
 
 int main(int argc, char* argv[])
 {
-    if (argc == 1) {
-        INFO("Usage: chip8 <rom>");
-        return 1;
+    if (argc == 3) {
+        RunEmulator(argv[1], argv[2]);
     }
-    Chip8App app("CHIP8 Emulator", 64, 32);
-    app.LoadRom(argv[1]);
-    app.Run();
+    else {
+        PrintUsage();
+    }
     return 0;
 }
