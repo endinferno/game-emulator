e7f7a9dcf2bee4b2e4ea9edfefda8d8ea3e8b755
Edward C <censhaofeng@sjtu.edu.cn>
2023-12-02 16:54:40 +0800

    chore: update format.sh script

--- script/format.sh
+++ script/format.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 clang-format -style=file -i Main.cpp
-find src -regex '.*\.\(cpp\|hpp\)' -exec clang-format -style=file -i {} \;
-find include -regex '.*\.\(cpp\|hpp\)' -exec clang-format -style=file -i {} \;
+find src -type f -name "*" -exec clang-format -style=file -i {} \;
+find include -type f -name "*" -exec clang-format -style=file -i {} \;
